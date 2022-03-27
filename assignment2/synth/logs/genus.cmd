4a2f9b4845fac1ec86fabd03e391f303b3067cf2
Dinesh Joshi <97094135+DJ-dineshjoshi@users.noreply.github.com>
2022-03-28 00:53:01 +0530

    Add files via upload

---
+++ assignment2/synth/logs/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 19.12-s121_1, built Dec  3 2019 15:07:17
+
+# Date: Tue Feb 01 22:11:37 2022
+# Host: dirac1 (x86_64 w/Linux 4.9.0-4-amd64) (1core*24cpus*1physical cpu*Intel(R) Xeon(R) CPU E5-2650 v2 @ 2.60GHz 20480KB)
+# OS:   Unsupported OS as /etc does not have release info
+
+source ../scripts/genus_script.tcl
---
+++ assignment2/synth/scripts/genus_script.tcl
@@ -0,0 +1,45 @@
+# Design path declaration----------
+# We need to declare teh path of .lib files and the RTL files.
+set_attribute lib_search_path      {/afs/iitd.ac.in/user/j/jv/jvl202215/elp736/assignments/assignment2/synth/libs/}
+set_attribute init_hdl_search_path {/afs/iitd.ac.in/user/j/jv/jvl202215/elp736/assignments/assignment2/rtl/}
+set_attribute library [list uk65lscllmvbbr_100c25_tc_ccs.lib]
+set myfiles [list fifo.v fifomem.v rptr_empty.v sync_r2w.v sync_w2r.v wptr_full.v]
+
+# Declaring verbosity level as 6. 6 is the tool recommended value.
+set_attribute information_level 6
+#----------------------------------
+
+# Read the RTL files --------------
+set basename fifo
+set runname synthesis_report
+read_hdl ${myfiles}
+elaborate ${basename}
+
+# Reading SDC file  ---------------
+read_sdc ../sdc/${basename}.sdc
+#----------------------------------
+
+check_design -unresolved
+
+# Dumping netlist, sdc and sdf ---
+#syn_gen
+#write_hdl -mapped > ../netlist/${basename}_gen.v
+#syn_opt
+#write_hdl -mapped > ../netlist/${basename}_post_opt.v
+synthesize -to_mapped
+write_hdl -mapped > ../netlist/${basename}_mapped.v
+write_sdc > ../sdc/${basename}_output.sdc
+write_sdf > ../sdf/${basename}.sdf
+#----------------------------------
+
+# Writing the timing, area, power and qor reports
+report timing -num_paths 99999 > ${basename}_${runname}_timing.rep
+report gates > ${basename}_${runname}_gates.rep
+report power > ${basename}_${runname}_power.rep
+report qor > ${basename}_${runname}_qor.rep
+report timing -lint -verbose > ${basename}_${runname}_timinglint.rep
+check_design > ${basename}_${runname}_checkdesign.rep
+#----------------------------------
+
+#gui_show
+exit
