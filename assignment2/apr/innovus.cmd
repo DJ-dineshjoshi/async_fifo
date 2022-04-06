c402dc16a9770233ffbad6dc07642ff4da25bb60
Dinesh Joshi <97094135+DJ-dineshjoshi@users.noreply.github.com>
2022-04-07 04:54:51 +0530

    Add files via upload

--- assignment2/apr/innovus.cmd
+++ assignment2/apr/innovus.cmd
@@ -1,7 +1,7 @@
 #######################################################
 #                                                     
 #  Innovus Command Logging File                     
-#  Created on Sun Mar 27 23:29:11 2022                
+#  Created on Thu Apr  7 03:47:38 2022                
 #                                                     
 #######################################################
 
@@ -43,235 +43,139 @@ set distributed_client_message_echo 1
 set distributed_mmmc_disable_reports_auto_redirection 0
 set eco_post_client_restore_command {update_timing ; write_eco_opt_db ;}
 set init_gnd_net VSS
-set init_lef_file {../synth/lef/tf/uk65lscllmvbbr_6m1t0f.lef ../synth/lef/uk65lscllmvbbr.lef}
+set init_lef_file /afs/iitd.ac.in/user/j/jv/jvy197519/LEF_file/trial_gcd.lef
 set init_mmmc_file Default.view
 set init_pwr_net VDD
 set init_top_cell fifo
-set init_verilog ../synth/netlist/fifo_post_opt.v
+set init_verilog ../synth/netlist/fifo_opt.v
 set latch_time_borrow_mode max_borrow
 set pegDefaultResScaleFactor 1
 set pegDetailResScaleFactor 1
 set report_inactive_arcs_format {from to when arc_type sense reason}
 set tso_post_client_restore_command {update_timing ; write_eco_opt_db ;}
 init_design
-panPage -1 0
-panPage 0 -1
-panPage -1 0
-panPage 1 0
-panPage 1 0
-panPage 0 1
-panPage 0 -1
-panPage 0 1
-panPage 0 -1
-panPage 0 1
-definePartition -hinst fifomem -coreSpacing 0.0 0.0 0.0 0.0 -railWidth 0.0 -minPitchLeft 2 -minPitchRight 2 -minPitchTop 2 -minPitchBottom 2 -reservedLayer { 1 2 3 4 5 6 7} -pinLayerTop { 2 4 6} -pinLayerLeft { 3 5 7} -pinLayerBottom { 2 4 6} -pinLayerRight { 3 5 7} -placementHalo 0.0 0.0 0.0 0.0 -routingHalo 0.0 -routingHaloTopLayer 7 -routingHaloBottomLayer 1
-setDrawView fplan
-zoomOut
-zoomOut
-zoomIn
-zoomIn
-zoomOut
-panPage 1 0
-panPage -1 0
-panPage 0 1
-panPage 0 -1
-panPage 0 -1
-getCTSMode -engine -quiet
-setDrawView place
-setDrawView place
-setDrawView ameba
-zoomIn
-zoomIn
-panPage 0 1
-panPage 0 1
-panPage 0 1
-panPage 0 1
-panPage 0 1
-panPage 0 1
-panPage 0 1
-panPage 0 1
-zoomOut
-panPage 0 1
-panPage 0 1
-panPage 0 1
-panPage 0 -1
-panPage 0 -1
-panPage 0 -1
-panPage 0 1
-zoomOut
-setDrawView place
-zoomIn
-setPlaceMode -fp false
-placeDesign
-setDrawView fplan
-setDrawView ameba
-panPage 0 -1
-panPage 0 -1
-panPage 0 1
-panPage 0 1
-zoomOut
-panPage 0 -1
-panPage 1 0
-setDrawView place
-setDrawView fplan
-selectObject Module fifomem
-zoomIn
-fit
-setDrawView fplan
-setDrawView place
-setDrawView fplan
-setDrawView fplan
-deselectAll
-selectObject Module fifomem
-deselectAll
-selectObject Module fifomem
-deselectAll
 getIoFlowFlag
 setIoFlowFlag 0
-floorPlan -site CORE -r 0.99684904039 0.695252 10.0 10.0 10.0 10.0
+floorPlan -coreMarginsBy die -site CORE -r 1.0 0.7 4.5 4.5 4.5 4.5
 uiSetTool select
 getIoFlowFlag
 fit
-setDrawView fplan
-setDrawView fplan
-selectObject Module fifomem
-deselectAll
-selectObject Module fifomem
-deselectAll
-selectObject Module fifomem
-deselectAll
-selectObject Module fifomem
-deselectAll
-getPinAssignMode -pinEditInBatch -quiet
-gui_select -rect {0.795 33.306 -1.403 17.093}
-gui_select -rect {-3.235 34.039 2.994 18.466}
-setPinAssignMode -pinEditInBatch true
-editPin -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Right -layer 1 -spreadType start -spacing 0.2 -start 0.0 0.0 -pin {{rdata[0]} {rdata[1]} {rdata[2]} {rdata[3]} {rdata[4]} {rdata[5]} {rdata[6]} {rdata[7]} wfull rempty}
-setPinAssignMode -pinEditInBatch false
-gui_select -rect {70.231 74.070 78.475 66.008}
-getCTSMode -engine -quiet
-getPinAssignMode -pinEditInBatch -quiet
-setPinAssignMode -pinEditInBatch true
-editPin -pinWidth 0.09 -pinDepth 0.47 -fixOverlap 1 -spreadDirection clockwise -side Right -layer 1 -spreadType side -pin {{rdata[0]} {rdata[1]} {rdata[2]} {rdata[3]} {rdata[4]} {rdata[5]} {rdata[6]} {rdata[7]} rempty wfull}
-setPinAssignMode -pinEditInBatch false
-deselectAll
-gui_select -rect {86.169 8.757 60.520 24.054}
-gui_select -rect {69.681 18.649 75.636 13.337}
-deselectAll
-zoomIn
-panPage 0 -1
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage -1 0
-panPage -1 0
-panPage -1 0
-panPage -1 0
-panPage -1 0
-panPage -1 0
-panPage -1 0
-panPage -1 0
-panPage -1 0
-panPage 1 0
-panPage 1 0
-panPage 1 0
-zoomIn
-zoomIn
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage 1 0
-panPage 0 -1
-panPage 0 -1
-panPage 0 -1
-panPage 0 -1
-zoomSelected
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomIn
-zoomIn
-zoomIn
-zoomIn
-panPage 0 1
-panPage -1 0
-panPage 0 -1
-getIoFlowFlag
-getPinAssignMode -pinEditInBatch -quiet
-set ptngSprNoRefreshPins 1
-setPtnPinStatus -cell fifo -pin rclk -status unplaced -silent
-setPtnPinStatus -cell fifo -pin rinc -status unplaced -silent
-setPtnPinStatus -cell fifo -pin rrst_n -status unplaced -silent
-setPtnPinStatus -cell fifo -pin wclk -status unplaced -silent
-setPtnPinStatus -cell fifo -pin winc -status unplaced -silent
-setPtnPinStatus -cell fifo -pin wrst_n -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[0]} -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[1]} -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[2]} -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[3]} -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[4]} -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[5]} -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[6]} -status unplaced -silent
-setPtnPinStatus -cell fifo -pin {wdata[7]} -status unplaced -silent
-set ptngSprNoRefreshPins 0
-ptnSprRefreshPinsAndBlockages
-setPinAssignMode -pinEditInBatch false
-getIoFlowFlag
+set sprCreateIeRingOffset 1.0
+set sprCreateIeRingThreshold 1.0
+set sprCreateIeRingJogDistance 1.0
+set sprCreateIeRingLayers {}
+set sprCreateIeRingOffset 1.0
+set sprCreateIeRingThreshold 1.0
+set sprCreateIeRingJogDistance 1.0
+set sprCreateIeRingLayers {}
+set sprCreateIeStripeWidth 10.0
+set sprCreateIeStripeThreshold 1.0
+set sprCreateIeStripeWidth 10.0
+set sprCreateIeStripeThreshold 1.0
+set sprCreateIeRingOffset 1.0
+set sprCreateIeRingThreshold 1.0
+set sprCreateIeRingJogDistance 1.0
+set sprCreateIeRingLayers {}
+set sprCreateIeStripeWidth 10.0
+set sprCreateIeStripeThreshold 1.0
+setAddRingMode -ring_target default -extend_over_row 0 -ignore_rows 0 -avoid_short 0 -skip_crossing_trunks none -stacked_via_top_layer AL_RDL -stacked_via_bottom_layer ME1 -via_using_exact_crossover_size 1 -orthogonal_only true -skip_via_on_pin {  standardcell } -skip_via_on_wire_shape {  noshape }
+addRing -nets {VDD VSS} -type core_rings -follow core -layer {top ME7 bottom ME1 left ME2 right ME2} -width {top 0.7 bottom 0.7 left 0.7 right 0.7} -spacing {top 0.985 bottom 0.985 left 0.985 right 0.985} -offset {top 1.8 bottom 1.8 left 1.8 right 1.8} -center 1 -extend_corner {} -threshold 0 -jog_distance 0 -snap_wire_center_to_grid None
+setAddRingMode -ring_target default -extend_over_row 0 -ignore_rows 0 -avoid_short 0 -skip_crossing_trunks none -stacked_via_top_layer AL_RDL -stacked_via_bottom_layer ME1 -via_using_exact_crossover_size 1 -orthogonal_only true -skip_via_on_pin {  standardcell } -skip_via_on_wire_shape {  noshape }
+addRing -nets {VDD VSS} -type core_rings -follow core -layer {top ME7 bottom ME7 left ME8 right ME8} -width {top 0.7 bottom 0.7 left 0.7 right 0.7} -spacing {top 0.985 bottom 0.985 left 0.985 right 0.985} -offset {top 1.8 bottom 1.8 left 1.8 right 1.8} -center 1 -extend_corner {} -threshold 0 -jog_distance 0 -snap_wire_center_to_grid None
+set sprCreateIeRingOffset 1.0
+set sprCreateIeRingThreshold 1.0
+set sprCreateIeRingJogDistance 1.0
+set sprCreateIeRingLayers {}
+set sprCreateIeRingOffset 1.0
+set sprCreateIeRingThreshold 1.0
+set sprCreateIeRingJogDistance 1.0
+set sprCreateIeRingLayers {}
+set sprCreateIeStripeWidth 10.0
+set sprCreateIeStripeThreshold 1.0
+set sprCreateIeStripeWidth 10.0
+set sprCreateIeStripeThreshold 1.0
+set sprCreateIeRingOffset 1.0
+set sprCreateIeRingThreshold 1.0
+set sprCreateIeRingJogDistance 1.0
+set sprCreateIeRingLayers {}
+set sprCreateIeStripeWidth 10.0
+set sprCreateIeStripeThreshold 1.0
+setAddStripeMode -ignore_block_check false -break_at none -route_over_rows_only false -rows_without_stripes_only false -extend_to_closest_target none -stop_at_last_wire_for_area false -partial_set_thru_domain false -ignore_nondefault_domains false -trim_antenna_back_to_shape none -spacing_type edge_to_edge -spacing_from_block 0 -stripe_min_length 0 -stacked_via_top_layer AL_RDL -stacked_via_bottom_layer ME1 -via_using_exact_crossover_size false -split_vias false -orthogonal_only true -allow_jog { padcore_ring  block_ring }
+addStripe -nets {VDD VSS} -layer ME6 -direction vertical -width 0.3 -spacing 0.4 -set_to_set_distance 2.5 -start_from left -start_offset 0.4 -switch_layer_over_obs false -max_same_layer_jog_length 2 -padcore_ring_top_layer_limit AL_RDL -padcore_ring_bottom_layer_limit ME1 -block_ring_top_layer_limit AL_RDL -block_ring_bottom_layer_limit ME1 -use_wire_group 0 -snap_wire_center_to_grid None -skip_via_on_pin {  standardcell } -skip_via_on_wire_shape {  noshape }
 getPinAssignMode -pinEditInBatch -quiet
 setPinAssignMode -pinEditInBatch true
-editPin -fixOverlap 1 -spreadDirection clockwise -side Left -layer 1 -spreadType side -pin {rclk rinc rrst_n wclk {wdata[0]} {wdata[1]} {wdata[2]} {wdata[3]} {wdata[4]} {wdata[5]} {wdata[6]} {wdata[7]} winc wrst_n}
+editPin -fixOverlap 1 -spreadDirection clockwise -side Right -layer 3 -spreadType side -pin {{rdata[0]} {rdata[1]} {rdata[2]} {rdata[3]} {rdata[4]} {rdata[5]} {rdata[6]} {rdata[7]} rempty wfull}
 setPinAssignMode -pinEditInBatch false
-setDrawView fplan
-setDrawView ameba
-setDrawView place
-setDrawView fplan
-setDrawView fplan
-setDrawView ameba
-panPage 0 1
-panPage 0 -1
-panPage 0 1
-panPage 0 -1
-panPage 0 1
-panPage 0 -1
-gui_select -rect {59.843 41.803 58.011 39.605}
-deselectAll
-gui_select -rect {59.751 35.300 46.285 30.719}
-deselectAll
-setDrawView ameba
-setDrawView ameba
-setDrawView place
-setDrawView fplan
-panPage 0 1
-panPage 0 -1
-panPage 0 1
-zoomOut
-panPage 0 1
-panPage 0 -1
-panPage 0 -1
-zoomIn
-zoomIn
-zoomOut
-panPage 0 1
-panPage 0 1
-panPage 0 1
-zoomBox 83.385 38.699 84.210 35.035
-zoomBox 86.403 36.248 86.529 36.514
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomOut
-zoomIn
-zoomOut
+getPinAssignMode -pinEditInBatch -quiet
+setPinAssignMode -pinEditInBatch true
+editPin -fixOverlap 1 -spreadDirection clockwise -side Left -layer 3 -spreadType side -pin {wrst_n winc rinc rrst_n wclk {wdata[0]} {wdata[1]} {wdata[2]} {wdata[3]} {wdata[4]} {wdata[5]} {wdata[6]} {wdata[7]} rclk}
+setPinAssignMode -pinEditInBatch false
+setPlaceMode -fp false
+placeDesign
+place_opt_design
+setEndCapMode -reset
+setEndCapMode -boundary_tap false
+setUsefulSkewMode -maxSkew false -noBoundary false -useCells {DEL4M4R DEL4M1R DEL3M4R DEL3M1R DEL2M4R DEL2M1R DEL1M4R DEL1M1R CKBUFM8R CKBUFM6R CKBUFM4R CKBUFM48R CKBUFM40R CKBUFM3R CKBUFM32R CKBUFM2R CKBUFM26RA CKBUFM24R CKBUFM22RA CKBUFM20R CKBUFM1R CKBUFM16R CKBUFM12R BUFM8R BUFM6R BUFM5R BUFM4R BUFM48RA BUFM40RA BUFM3R BUFM32RA BUFM2R BUFM26RA BUFM24R BUFM22RA BUFM20R BUFM18R BUFM16R BUFM14R BUFM12R BUFM10R INVM8R INVM6R INVM5R INVM4R INVM48R INVM40R INVM3R INVM32R INVM2R INVM26RA INVM24R INVM22RA INVM20R INVM1R INVM18R INVM16R INVM14R INVM12R INVM10R CKINVM8R CKINVM6R CKINVM4R CKINVM48R CKINVM40R CKINVM3R CKINVM32R CKINVM2R CKINVM26RA CKINVM24R CKINVM22RA CKINVM20R CKINVM1R CKINVM16R CKINVM12R} -maxAllowedDelay 1
+setPlaceMode -reset
+setPlaceMode -congEffort auto -timingDriven 1 -modulePlan 1 -clkGateAware 1 -powerDriven 0 -ignoreScan 1 -reorderScan 0 -ignoreSpare 0 -placeIOPins 0 -moduleAwareSpare 0 -preserveRouting 0 -rmAffectedRouting 0 -checkRoute 0 -swapEEQ 0
+setPlaceMode -fp false
+placeDesign
+place_opt_design
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -preCTS -idealClock -pathReports -slackReports -numPaths 50 -prefix fifo_preCTS -outDir timingReports
+selectWire 34.2500 32.3500 34.3500 47.8500 4 n_162
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -preCTS -hold -idealClock -pathReports -slackReports -numPaths 50 -prefix fifo_preCTS -outDir timingReports
+create_ccopt_clock_tree_spec
+ccopt_design
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postCTS -pathReports -drvReports -slackReports -numPaths 50 -prefix fifo_postCTS -outDir timingReports
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postCTS -hold -pathReports -slackReports -numPaths 50 -prefix fifo_postCTS -outDir timingReports
+setOptMode -fixCap true -fixTran true -fixFanoutLoad false
+optDesign -postCTS -hold
+setNanoRouteMode -quiet -timingEngine {}
+setNanoRouteMode -quiet -routeWithTimingDriven 1
+setNanoRouteMode -quiet -routeWithSiDriven 1
+setNanoRouteMode -quiet -routeWithSiPostRouteFix 0
+setNanoRouteMode -quiet -drouteStartIteration default
+setNanoRouteMode -quiet -routeTopRoutingLayer default
+setNanoRouteMode -quiet -routeBottomRoutingLayer default
+setNanoRouteMode -quiet -drouteEndIteration default
+setNanoRouteMode -quiet -routeWithTimingDriven true
+setNanoRouteMode -quiet -routeWithSiDriven true
+routeDesign -globalDetail
+setAnalysisMode -analysisType onChipVariation
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postRoute -pathReports -slackReports -numPaths 50 -prefix fifo_postRoute -outDir timingReports
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postRoute -hold -pathReports -slackReports -numPaths 50 -prefix fifo_postRoute -outDir timingReports
+setOptMode -fixCap true -fixTran true -fixFanoutLoad false
+optDesign -postRoute
+optDesign -postRoute -hold
+saveDesign fifo_postRoute
+saveFPlan fifo_post_route_floorplan.fp
+rcOut -spf fifo.spf -rc_corner Default_rc_corner
+rcOut -spef fifo.spef -rc_corner Default_rc_corner
+setVerifyGeometryMode -area { 0 0 0 0 } -minWidth true -minSpacing true -minArea true -sameNet true -short true -overlap true -offRGrid false -offMGrid true -mergedMGridCheck true -minHole true -implantCheck true -minimumCut true -minStep true -viaEnclosure true -antenna false -insuffMetalOverlap true -pinInBlkg false -diffCellViol true -sameCellViol false -padFillerCellsOverlap true -routingBlkgPinOverlap true -routingCellBlkgOverlap true -regRoutingOnly false -stackedViasOnRegNet false -wireExt true -useNonDefaultSpacing false -maxWidth true -maxNonPrefLength -1 -error 1000
+verifyGeometry
+setVerifyGeometryMode -area { 0 0 0 0 }
+get_verify_drc_mode -disable_rules -quiet
+get_verify_drc_mode -quiet -area
+get_verify_drc_mode -quiet -layer_range
+get_verify_drc_mode -check_implant -quiet
+get_verify_drc_mode -check_implant_across_rows -quiet
+get_verify_drc_mode -check_ndr_spacing -quiet
+get_verify_drc_mode -check_only -quiet
+get_verify_drc_mode -check_same_via_cell -quiet
+get_verify_drc_mode -exclude_pg_net -quiet
+get_verify_drc_mode -ignore_trial_route -quiet
+get_verify_drc_mode -max_wrong_way_halo -quiet
+get_verify_drc_mode -use_min_spacing_on_block_obs -quiet
+get_verify_drc_mode -limit -quiet
+set_verify_drc_mode -disable_rules {} -check_implant true -check_implant_across_rows false -check_ndr_spacing false -check_same_via_cell false -exclude_pg_net false -ignore_trial_route false -report fifo.drc.rpt -limit 1000
+verify_drc
+set_verify_drc_mode -area {0 0 0 0}
+verifyConnectivity -type all -error 1000 -warning 50
+saveFPlan fifo_floorplan
+saveDesign fifo_final
